2985577a9edad3bd52cf9c3837011239b938a75a
imranhwebdev <alimrancv870@gmail.com>
2024-07-14 12:21:29 +0600

    titleupdate

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -11,7 +11,7 @@ import sociallogo6 from "../assets/img/pancakeicon.svg";
 import heroshap2 from "../assets/img/hero-top-left-star.png";
 import heroshap4 from "../assets/img/hero-right-shap.png";
 export default function Banner() {
-  const title = "$Cosmo";
+  const title = "$COSMO";
   const desc = "Mixi all meme coins and their communities together and change the memeverse with the Mixi cosmo cat. Love it, build it";
   const SocialItems = [
     {
